bffe653bfb6054a606086f20ef711325c8be5745
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 02:10:14 -0400

    Update last2.py

--- 03_py/warmup-2/last2.py
+++ 03_py/warmup-2/last2.py
@@ -4,6 +4,7 @@
 #2022-09-22
 #time spent: 3.0 hrs
 
+#Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
 def last2(str):
   substring = str[len(str) - 2 :]
   answer = 0
@@ -13,3 +14,7 @@ def last2(str):
       answer += 1
     counter += 1
   return answer
+  
+print(last2('hixxhi'), "→ 1")
+print(last2('xaxxaxaxx'), "→ 1")
+print(last2('axxxaaxx'), "→ 2")
