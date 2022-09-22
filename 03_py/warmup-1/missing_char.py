87e7fdc5aaec638459834ea79c77865e3c8ed28d
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:30:06 -0400

    Update missing_char.py

--- 03_py/warmup-1/missing_char.py
+++ 03_py/warmup-1/missing_char.py
@@ -1,2 +1,8 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def missing_char(str, n):
   return str[0:n] + str[n+1:len(str)]
