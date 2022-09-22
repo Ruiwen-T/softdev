1e60e5e9f2280acb6d304106a8ba8d54b3c14ea6
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:26:55 -0400

    Update centered_average.py

--- 03_py/list-2/centered_average.py
+++ 03_py/list-2/centered_average.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def centered_average(nums):
   ans = 0
   high = nums[0]
