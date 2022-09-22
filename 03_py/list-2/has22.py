5de3164781e9ec77892629ba29b58457cc2845cc
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:55:22 -0400

    Update has22.py

--- 03_py/list-2/has22.py
+++ 03_py/list-2/has22.py
@@ -9,3 +9,7 @@ def has22(nums):
     if nums[i] == 2 and nums[i+1] ==2:
       return True
   return False
+
+print(has22([1, 2, 2]), "→ True")
+print(has22([1, 2, 1, 2]), "→ False")
+print(has22([2, 1, 2]), "→ False")
