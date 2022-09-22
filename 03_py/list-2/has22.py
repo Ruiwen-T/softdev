8aa5052b57d606efe09afd2a44bd078b3dc826dd
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:28:30 -0400

    Update has22.py

--- 03_py/list-2/has22.py
+++ 03_py/list-2/has22.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def has22(nums):
   for i in range(len(nums)-1):
     if nums[i] == 2 and nums[i+1] ==2:
