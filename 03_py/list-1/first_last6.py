aead60d30eacbe75095587b1b6ff5ace435812e6
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:24:24 -0400

    Update first_last6.py

--- 03_py/list-1/first_last6.py
+++ 03_py/list-1/first_last6.py
@@ -1,2 +1,8 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def first_last6(nums):
   return nums[0] == 6 or nums[len(nums) - 1] == 6
