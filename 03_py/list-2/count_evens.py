8e875843652418284dea01085a2dd95f47a4a4c8
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:53:56 -0400

    Update count_evens.py

--- 03_py/list-2/count_evens.py
+++ 03_py/list-2/count_evens.py
@@ -10,3 +10,8 @@ def count_evens(nums):
     if i % 2 == 0:
       evens += 1
   return evens
+
+print(count_evens([2, 1, 2, 3, 4]), "→ 3")
+print(count_evens([2, 2, 0]), "→ 3")
+print(count_evens([1, 3, 5]), "→ 0")
+
