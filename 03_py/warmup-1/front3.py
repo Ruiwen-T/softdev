a1b68ce9aa9bf1833fd2e0c2bc889b395c4b9ef2
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:29:29 -0400

    Update front3.py

--- 03_py/warmup-1/front3.py
+++ 03_py/warmup-1/front3.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def front3(str):
   if len(str) < 3:
     return str * 3
