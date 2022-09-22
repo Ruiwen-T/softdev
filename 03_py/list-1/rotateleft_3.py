c6d0c067637295e6ebab105f096fd9e56f1e2596
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:25:46 -0400

    Update rotateleft_3.py

--- 03_py/list-1/rotateleft_3.py
+++ 03_py/list-1/rotateleft_3.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def rotate_left3(nums):
   new_nums = nums
   first = nums[0]
