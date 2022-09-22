567d4620fa5c57d0e4af36ea91c1070f3d46b41b
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 02:04:24 -0400

    Update front3.py

--- 03_py/warmup-1/front3.py
+++ 03_py/warmup-1/front3.py
@@ -9,3 +9,7 @@ def front3(str):
     return str * 3
   else:
     return str[:3] *3
+  
+print(front3('Java'), "→ 'JavJavJav'")
+print(front3('Chocolate'), "→ 'ChoChoCho'")
+print(front3('abc'), "→ 'abcabcabc'")
