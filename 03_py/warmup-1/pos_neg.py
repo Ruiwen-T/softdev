8f12628c4b76600d92af9758b4112ccd1fbaa62b
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:31:06 -0400

    Update pos_neg.py

--- 03_py/warmup-1/pos_neg.py
+++ 03_py/warmup-1/pos_neg.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def pos_neg(a, b, negative):
   if negative == True:
     return a<0 and b<0
