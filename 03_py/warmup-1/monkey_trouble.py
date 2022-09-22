d90b26e3ac606465f4073b1eb178c0394b9c7a80
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:59:04 -0400

    Update monkey_trouble.py

--- 03_py/warmup-1/monkey_trouble.py
+++ 03_py/warmup-1/monkey_trouble.py
@@ -9,3 +9,7 @@ def monkey_trouble(a_smile, b_smile):
     return False
   else:
     return True
+  
+print(monkey_trouble(True, True), "→ True")
+print(monkey_trouble(False, False), "→ True")
+print(monkey_trouble(True, False), "→ False")
