a3131fa1361ea37115fbdb0b6aeb7814e1293cba
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:24:11 -0400

    Update common_end.py

--- 03_py/list-1/common_end.py
+++ 03_py/list-1/common_end.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def common_end(a, b):
   if a[0] == b[0]:
     return True
