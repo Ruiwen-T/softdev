b88c31fad579f69570a7ce01b69340fedfb55ee8
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:51:37 -0400

    Update big_diff.py

--- 03_py/list-2/big_diff.py
+++ 03_py/list-2/big_diff.py
@@ -13,3 +13,7 @@ def big_diff(nums):
     if max(current_max, i) == i:
       current_max = i
   return current_max - current_min
+
+print(big_diff([10, 3, 5, 6]), "→ 7")
+print(big_diff([7, 2, 10, 9]), "→ 8")
+print(big_diff([2, 10, 7, 2]), "→ 8")
