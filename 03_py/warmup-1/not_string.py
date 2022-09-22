d9b4b88509c40fcaa79930aa40c7a2edd70c5981
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 02:03:44 -0400

    Update not_string.py

--- 03_py/warmup-1/not_string.py
+++ 03_py/warmup-1/not_string.py
@@ -8,3 +8,7 @@ def not_string(str):
   if str[0:3] == "not":
     return str
   return "not" + " " + str
+
+print(not_string('candy'), "→ 'not candy'")
+print(not_string('x'), "→ 'not x'")
+print(not_string('not bad'), "→ 'not bad'")
