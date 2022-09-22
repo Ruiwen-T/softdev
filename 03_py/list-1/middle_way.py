42c1806714f13c528a3c03259c52b6299d94ad16
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:25:22 -0400

    Update middle_way.py

--- 03_py/list-1/middle_way.py
+++ 03_py/list-1/middle_way.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def middle_way(a, b):
   arr = [0,0]
   arr[0] = a[1]
