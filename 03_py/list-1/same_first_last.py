a690f9c173fbfbfbc30a71495ee757abc2b6cb49
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:40:27 -0400

    Update same_first_last.py

--- 03_py/list-1/same_first_last.py
+++ 03_py/list-1/same_first_last.py
@@ -9,3 +9,7 @@ def same_first_last(nums):
     if nums[0] == nums[len(nums) - 1]:
       return True
   return False
+
+print(same_first_last([1, 2, 3]), "→ False")
+print(same_first_last([1, 2, 3, 1]), "→ True")
+print(same_first_last([1, 2, 1]), "→ True")
