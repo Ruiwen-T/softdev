5500d470abe7fecac59cbd34aff213f6c9168699
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 02:18:27 -0400

    Update count_evens.py

--- 03_py/list-2/count_evens.py
+++ 03_py/list-2/count_evens.py
@@ -4,6 +4,7 @@
 #2022-09-22
 #time spent: 3.0 hrs
 
+#Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
 def count_evens(nums):
   evens = 0
   for i in nums:
