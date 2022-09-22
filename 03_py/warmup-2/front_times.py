2bda65fe3e1aabad4871da9aa1ff0e4aacb7a541
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:32:18 -0400

    Update front_times.py

--- 03_py/warmup-2/front_times.py
+++ 03_py/warmup-2/front_times.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def front_times(str, n):
   if len(str) < 3:
     return str * n
