2ebab45b945ae66cc3fb1f840cc4cbe0a842ca61
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 02:20:07 -0400

    Update has22.py

--- 03_py/list-2/has22.py
+++ 03_py/list-2/has22.py
@@ -4,6 +4,7 @@
 #2022-09-22
 #time spent: 3.0 hrs
 
+#Given an array of ints, return True if the array contains a 2 next to a 2 somewhere.
 def has22(nums):
   for i in range(len(nums)-1):
     if nums[i] == 2 and nums[i+1] ==2:
