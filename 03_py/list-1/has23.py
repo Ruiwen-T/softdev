f53b8f4e11aa151f6261b4152c972ba4c1c401b8
Ruiwen-T <you@example.com>
2022-09-22 01:49:58 -0400

    + has23

---
+++ 03_py/list-1/has23.py
@@ -0,0 +1,15 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
+def has23(nums):
+  for i in nums:
+    if i==2 or i==3:
+      return True
+  return False
+
+has23([2, 5]) → True
+has23([4, 3]) → True
+has23([4, 5]) → False
