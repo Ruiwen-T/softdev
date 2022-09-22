d6832c2cd51539bb185c1f6c49bb2666f2cc2769
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 02:00:30 -0400

    Update pos_neg.py

--- 03_py/warmup-1/pos_neg.py
+++ 03_py/warmup-1/pos_neg.py
@@ -8,3 +8,7 @@ def pos_neg(a, b, negative):
   if negative == True:
     return a<0 and b<0
   return (a<0 and b>0) or (a>0 and b<0)
+
+print(pos_neg(1, -1, False), "→ True")
+print(pos_neg(-1, 1, False), "→ True")
+print(pos_neg(-4, -5, True), "→ True")
