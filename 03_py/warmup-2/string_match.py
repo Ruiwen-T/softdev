17dda2263d24d73cf14876a9aef1ed713fb2901f
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:32:54 -0400

    Update string_match.py

--- 03_py/warmup-2/string_match.py
+++ 03_py/warmup-2/string_match.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def string_match(a, b):
   string1 = str(a)
   string2 = str(b)
