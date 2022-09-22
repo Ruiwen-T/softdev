40f5708e9fd867896bed591021250f58c00d601f
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:31:18 -0400

    Update sleep_in.py

--- 03_py/warmup-1/sleep_in.py
+++ 03_py/warmup-1/sleep_in.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def sleep_in(weekday, vacation):
   if vacation == True or weekday == False:
     return True
