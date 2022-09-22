0311f21d9010577c32460a52672d857c1be455b9
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:29:18 -0400

    Update diff21.py

--- 03_py/warmup-1/diff21.py
+++ 03_py/warmup-1/diff21.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def diff21(n):
   if n>21:
     return 2 * (abs(n-21))
