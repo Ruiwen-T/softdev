a0394b05a1c56cea288fdda154471f6ce0203f3c
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:30:43 -0400

    Update not_string.py

--- 03_py/warmup-1/not_string.py
+++ 03_py/warmup-1/not_string.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def not_string(str):
   if str[0:3] == "not":
     return str
