59969e7ccfe02b6180b15dae6491841e1d23eb49
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:24:37 -0400

    Update make_ends.py

--- 03_py/list-1/make_ends.py
+++ 03_py/list-1/make_ends.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def make_ends(nums):
   arr = [0,0]
   arr[0] = nums[0]
