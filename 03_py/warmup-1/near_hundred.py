8368dfd762eeabefeb9afca77f6377379a530c37
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:30:31 -0400

    Update near_hundred.py

--- 03_py/warmup-1/near_hundred.py
+++ 03_py/warmup-1/near_hundred.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def near_hundred(n):
   if abs(100-n) <= 10 or abs(200-n) <= 10:
     return True
