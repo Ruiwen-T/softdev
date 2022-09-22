6686f9d1a61e6c7aa0eaee0043cc506de0463360
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:30:55 -0400

    Update parrot_trouble.py

--- 03_py/warmup-1/parrot_trouble.py
+++ 03_py/warmup-1/parrot_trouble.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def parrot_trouble(talking, hour):
   if hour<7 or hour>20:
     if talking == True:
