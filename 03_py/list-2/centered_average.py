da080fcf6bf62b05ea02385da96b1189a1efe19e
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:52:18 -0400

    Update centered_average.py

--- 03_py/list-2/centered_average.py
+++ 03_py/list-2/centered_average.py
@@ -13,3 +13,7 @@ def centered_average(nums):
     high = max(n, high)
     low = min(n, low)
   return (ans - high - low)/(len(nums)-2)
+
+print(centered_average([1, 2, 3, 4, 100]), "→ 3")
+print(centered_average([1, 1, 5, 5, 10, 8, 7]), "→ 5")
+print(centered_average([-10, -4, -2, -4, -2, 0]), "→ -3")
