502c5789c70ac95197f2599041b43ce07d8f2ed9
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 02:14:57 -0400

    Update monkey_trouble.py

--- 03_py/warmup-1/monkey_trouble.py
+++ 03_py/warmup-1/monkey_trouble.py
@@ -4,6 +4,7 @@
 #2022-09-22
 #time spent: 3.0 hrs
 
+#We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
 def monkey_trouble(a_smile, b_smile):
   if a_smile != b_smile:
     return False
