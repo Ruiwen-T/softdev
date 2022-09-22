59bd876d661eea9a6d9e4853c29c91064bb9793a
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:29:54 -0400

    Update makes10.py

--- 03_py/warmup-1/makes10.py
+++ 03_py/warmup-1/makes10.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def makes10(a, b):
   if a == 10 or b == 10 or a+b==10:
     return True
