e3637b6c4afab1a0f7cef385b0312f3a65935dd1
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 02:03:01 -0400

    Update makes10.py

--- 03_py/warmup-1/makes10.py
+++ 03_py/warmup-1/makes10.py
@@ -8,3 +8,7 @@ def makes10(a, b):
   if a == 10 or b == 10 or a+b==10:
     return True
   return False
+
+print(makes10(9, 10), "→ True")
+print(makes10(9, 9), "→ False")
+print(makes10(1, 9), "→ True")
