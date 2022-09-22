89638c79a0819347b1612e9779f11c20e45a896c
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:30:18 -0400

    Update monkey_trouble.py

--- 03_py/warmup-1/monkey_trouble.py
+++ 03_py/warmup-1/monkey_trouble.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def monkey_trouble(a_smile, b_smile):
   if a_smile != b_smile:
     return False
