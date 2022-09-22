5cad3c35dee15d7fc2053b2884ec33dbbb07e644
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:45:30 -0400

    Update rotateleft_3.py

--- 03_py/list-1/rotateleft_3.py
+++ 03_py/list-1/rotateleft_3.py
@@ -11,3 +11,7 @@ def rotate_left3(nums):
     new_nums[i] = nums[i+1]
   new_nums[len(nums)-1] = first
   return new_nums
+
+print(rotate_left3([1, 2, 3]), "→ [2, 3, 1]")
+print(rotate_left3([5, 11, 9]), "→ [11, 9, 5]")
+print(rotate_left3([7, 0, 0]), "→ [0, 0, 7]")
