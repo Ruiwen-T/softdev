c820505ffb7ed50966ca827b11996ff9ed23caff
Raven Tang <76086431+Ruiwen-T@users.noreply.github.com>
2022-09-22 01:26:43 -0400

    Update big_diff.py

--- 03_py/list-2/big_diff.py
+++ 03_py/list-2/big_diff.py
@@ -1,3 +1,9 @@
+#RUBBER DUCKS: Ivan Yeung, Jeffrey Zou, Raven (Ruiwen) Tang
+#SoftDev
+#CodingBat Exercises
+#2022-09-22
+#time spent: 3.0 hrs
+
 def big_diff(nums):
   current_min = nums[0]
   current_max = nums[0]
