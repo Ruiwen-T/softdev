25ee012cd2b8b02ae7cb3ddf53d4c2e9796b90d1
RuiwenT <ruiwentang@gmail.com>
2022-10-20 05:27:41 -0400

    fix app.py

--- 14_css/app.py
+++ 14_css/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def disp_page():
-    return render_template('static/index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
