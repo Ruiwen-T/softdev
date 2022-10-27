c5e4043c80c600f5f500d722c877745c43cc9a23
RuiwenT <ruiwentang@gmail.com>
2022-10-27 06:48:39 -0400

    finish 18

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -1,7 +1,8 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev  
-#skeleton/stub :: SQLITE3 BASICS
-#Oct 2022
+# Abid Talukder, Raven Tang, Craig Chen
+# SoftDev
+# K18: (Python+SQLite)3: A Mare Widge Made in Hebben
+# 2022-10-26
+# time spent: 0.8 Hours
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -17,7 +18,7 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
 
-delete_table ="DROP TABLE IF EXISTS students"
+delete_table ="DROP TABLE IF EXISTS students" # students table, delete if already exists
 c.execute(delete_table)
 create_table = "create table students(name text, age int, id int);"          # test SQL stmt in sqlite3 shell, save as string
 c.execute(create_table)    # run SQL statement
@@ -26,12 +27,44 @@ c.execute(create_table)    # run SQL statement
 with open('students.csv') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=',')
     line_count = 0
-    for row in csv_reader:
+    for row in csv_reader: 
         c.execute(f'insert into students values(\'{row["name"]}\',{row["age"]},{row["id"]});')
+
+delete_table ="DROP TABLE IF EXISTS courses"
+c.execute(delete_table)
+create_table = "create table courses(code text, mark int, id int);"          
+c.execute(create_table)    # run SQL statement
+
+# Opening CSV file for iteration
+with open('courses.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader: 
+        c.execute(f'insert into courses values(\'{row["code"]}\',{row["mark"]},{row["id"]});')
 
 #==========================================================
 
 db.commit() #save changes
+c.execute("select * from students;")
+students = (c.fetchall())
+
+print("name | age | id")
+print()
+for group in students:
+    print(f'{group[0]} | {group[1]} | {group[2]}')
+
+print()
+print("**********")
+print()
+
+c.execute("select * from courses;")
+courses = (c.fetchall())
+
+print("code | mark | id")
+print()
+for group in courses:
+    print(f'{group[0]} | {group[1]} | {group[2]}')
+
 db.close()  #close database
 
 
